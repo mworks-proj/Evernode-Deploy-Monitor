d32ae160e77bc6c085c5c127e28c611399ba5d60
Paolo Costa <genesis.one.seven.one@gmail.com>
2024-03-16 09:15:43 +0100

    heartbeat monitor implemented

--- transfer_funds.js
+++ transfer_funds.js
@@ -14,10 +14,15 @@ const lib = require('xrpl-accountlib');
 const { exit } = require('process');
 //const path = require('path')
 
+const fs = require('fs')
+const { createTransport } = require('nodemailer');
+
+
 //get variables from .env files
 //const env = require("dotenv").config({path:".env"});
 //const env = require("dotenv").config({path: __dirname + '.env'})
-const path = require('path')
+const path = require('path');
+const { ALPN_ENABLED } = require('constants');
 require('dotenv').config({ path: path.resolve(__dirname, '.env') })
 
 //accounts = your Node Wallets r Addresses 
@@ -33,12 +38,32 @@ const keypair = lib.derive.familySeed(secret)
 const xahaud = process.env.xahaud;
 const client = new XrplClient(xahaud);
 
+const minutes_from_last_heartbeat_alert_threshold =   process.env.minutes_from_last_heartbeat_alert_threshold;
+const alert_repeat_interval_in_minutes =   process.env.alert_repeat_interval_in_minutes;
+const xah_balance_threshold =   process.env.xah_balance_threshold;
+const refund_amount =   process.env.alert_minutes;
+
+const smtpKey =   process.env.smtpKey;
+const smtpEmail =   process.env.smtpEmail;
+
+const transporter = createTransport({
+  host: "smtp-relay.sendinblue.com",
+  port: 587,
+  auth: {
+      user: smtpEmail,
+      pass: smtpKey,
+  },
+});
+
+
+const heartbeatAccount =   process.env.heartbeatAccount;
+
 const wallets = []
 
 const myDate = new Date().toUTCString();
 
-console.log('Printing Account INFO...:', myDate);
-const main = async () => {
+
+const transfer_funds = async () => {
     for(const account of accounts) {
       //console.log('\nGetting ready...'); 
    
@@ -114,10 +139,133 @@ const main = async () => {
     
 
     } //end of for loop
-
-  console.log('Shutting down...');
-
-  client.close()
 }
 
-main()
+const monitor_heartbeat = async () => {
+    
+    for(const account of accounts)
+    {
+        await checkAccountHeartBeat(account);
+    }
+}
+
+function getMinutesBetweenDates(startDate, endDate) {
+  const diff = endDate - startDate;
+  return (diff / 60000);
+}
+
+async function checkAccountHeartBeat(account)
+  {
+    const path = './.' + account + '.txt'
+    var accountFailed = fs.existsSync(path);
+    var date_failure = new Date();
+    if(accountFailed)
+    {
+        date_failure = Date.parse(fs.readFileSync(path, 'utf8'));
+        
+        const diffMinutes = getMinutesBetweenDates(date_failure, new Date());
+        console.log(diffMinutes, alert_repeat_interval_in_minutes);
+        if(alert_repeat_interval_in_minutes > 0 && diffMinutes > alert_repeat_interval_in_minutes)
+        {
+            accountFailed = false;
+        }
+    } 
+    let marker = '';
+    const l = []; 
+    while (typeof marker === 'string') {
+    const  response  = await client.send({
+      "id": 2,
+      "command": "account_tx",
+      "account": account,
+      "ledger_index_min": -1,
+      "ledger_index_max": -1,
+    "binary": false,
+    "limit": 10,
+    "forward": false, marker: marker === '' ? undefined : marker 
+    })
+    marker = response?.marker === marker ? null : response?.marker
+    // It gets the last 10 transactions and looks for the last heartbeat
+    var i = 0;
+    for(var tIndex = 0; tIndex<10; tIndex++)
+    { 
+        var transaction = response.transactions[tIndex];
+        
+          if(transaction.tx.Destination == heartbeatAccount ){
+              var utcMilliseconds =1000 * (transaction.tx.date + 946684800);
+              var transactionDate = new Date(0); // The 0 there is the key, which sets the date to the epoch
+              var date = new Date();
+              var now_utc = Date.UTC(date.getUTCFullYear(), date.getUTCMonth(),
+                  date.getUTCDate(), date.getUTCHours(),
+                  date.getUTCMinutes(), date.getUTCSeconds());
+              if(now_utc - utcMilliseconds > 1000 * 60 * minutes_from_last_heartbeat_alert_threshold)
+              {
+                  console.log("Handling failure for too hold heartbeat transaction, account failed = " + accountFailed );
+                  await handleFailure(account, accountFailed, path);
+                  return;
+              }
+              console.log("System running regularly " + account + "");
+              if(fs.existsSync(path)) 
+                  {
+                      await sendSuccess(account);
+                      fs.rmSync(path);
+                  }
+                  return;
+          }
+    }
+    console.log("Handling failure for no heartbeat transactions, account failed = " + accountFailed);
+    await handleFailure(account, accountFailed, path);
+  }
+}
+
+async function handleFailure(account, accountFailed, path)
+{
+   if(!accountFailed) 
+   {
+      await sendFailure(account);
+      fs.writeFileSync(path, new Date().toString())
+   }
+   console.log("ALERT, SYSTEM STOPPED " + account);
+}
+
+  async function sendFailure(account)
+  {
+      var subject = "Failure in Evernode heartbeat";
+      var text = "Failure in retrieving Evernode heartbeat for account " + account;
+      await sendMail(subject, text);
+  }
+
+  async function sendSuccess(account)
+  {
+    var subject = "Evernode heartbeat restored";
+    var text = "Evernode heartbeat restored in account " + account;
+    await sendMail(subject, text);
+  }
+
+  async function sendMail(subject, text)
+  {
+      var mailOptions = {
+          from: smtpEmail,
+          to: smtpEmail,
+          subject: subject,
+          text: text
+      };
+      console.log("SENDING MAIL " + JSON.stringify(mailOptions));
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    }); 
+  }
+
+  const main = async () => {
+    await transfer_funds();
+    await monitor_heartbeat();
+    console.log('Shutting down...');
+
+    client.close()
+  };
+
+  main()
+
